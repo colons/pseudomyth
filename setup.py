31892f7dd9aadbfb10af522529a2b6a5b80580dc
Iain Dawson <iain@colons.co>
2015-01-20 22:27:50 +0000

    Be 1.0.6.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/colons/pseudomyth',
     author='Iain Dawson',
     author_email='pypi@colons.co',
-    version='1.0.5',
+    version='1.0.6',
     license="BSD",
     platforms=['any'],
     packages=['pseudomyth'],
